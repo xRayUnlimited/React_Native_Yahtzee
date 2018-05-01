45dd400969030f104b453a51b6e23ee235c67f91
Ray Suyoto <ray.suy@icloud.com>
2018-05-01 14:16:24 -0600

    working game update

--- components/ScoreCard.js
+++ components/ScoreCard.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import ScoreSection from './ScoreSection';
 import { Grid, Col } from 'react-native-easy-grid';
+import { H1, View } from 'native-base';
+import { connect } from 'react-redux';
 
-const ScoreCard = () => (
-  <Grid>
-    <Col>
-      <ScoreSection label="Upper" />
-    </Col>
-    <Col>
-      <ScoreSection label="Lower" />
-    </Col>
-  </Grid>
+const calcTotals = (scores) => {
+  return scores.map( s => s.score )
+    .reduce( (total, val) => {
+      return total + val;
+    }, 0)
+}
+
+const styles = {
+  total: {
+    alignSelf: "center",
+    margin: 20,
+  },
+}
+
+const ScoreCard = ({ currentGame: { scores } }) => (
+  <View>
+    <Grid>
+      <Col>
+        <ScoreSection label="Upper" />
+      </Col>
+      <Col>
+        <ScoreSection label="Lower" />
+      </Col>
+    </Grid>
+    <H1 style={styles.total}>{`TOTAL: ${calcTotals(scores)}`}</H1>
+  </View>
 )
 
-export default ScoreCard;
+const mapStateToProps = (state) => {
+  return { currentGame: state.currentGame }
+}
+
+
+export default connect(mapStateToProps)(ScoreCard);
